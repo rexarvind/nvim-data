03bf438435ce140a6e4f535c1ef4e51a3875ba5d
Arvind Kumar <thatisarvind@gmail.com>
2022-10-31 10:08:27 +0530

    first commit

---
+++ lua/user/options.lua
@@ -0,0 +1,40 @@
+vim.opt.backup = false                          -- creates a backup file
+vim.opt.clipboard = "unnamedplus"               -- allows neovim to access system clipboard
+vim.opt.cmdheight = 1                           -- more space in neovim command line for displaying messages
+vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
+vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
+vim.opt.fileencoding = "utf-8"                  -- encoding written to a file
+vim.opt.hlsearch = true                         -- highlight all matches on previous search pattern
+vim.opt.ignorecase = true                       -- ignore case in search patterns
+vim.opt.mouse = "a"                             -- allow the mouse to be used in neovim
+vim.opt.pumheight = 10                          -- pop up menu height
+vim.opt.showmode = true                         -- show current mode like -- INSERT --
+vim.opt.showtabline = 0                         -- always show tabs
+vim.opt.smartcase = true                        -- smart case
+vim.opt.smartindent = true                      -- make indenting smarter again
+vim.opt.splitbelow = true                       -- force all horizontal splits to go below current window
+vim.opt.splitright = true                       -- force all vertical splits to go to right of current window
+vim.opt.swapfile = false                        -- creates a swap file
+vim.opt.termguicolors = true                    -- set terminal gui colors (most terminals support this)
+vim.opt.timeoutlen = 1000                       -- time to wait for mapped sequence to complete (in milliseconds)
+vim.opt.undofile = true                         -- enable persistent undo
+vim.opt.updatetime = 300                        -- faster completions (4000ms default)
+vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.opt.expandtab = true                        -- convert tabs to spaces
+vim.opt.shiftwidth = 2                          -- number of spaces inserted for each indentation
+vim.opt.tabstop = 2                             -- number of spaces for TAB key
+vim.opt.cursorline = true                       -- highlight current line
+vim.opt.number = true                           -- set numbered lines
+vim.opt.laststatus = 3                          -- only the last window will always have a status line
+vim.opt.showcmd = false                         -- hide (partial) command in the last line of the screen (for performance)
+vim.opt.ruler = false                           -- hide the line and column number of the cursor position
+vim.opt.numberwidth = 3                         -- minimal number of columns to use for the line number (default 4)
+vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
+vim.opt.wrap = false                            -- display lines as one long line
+vim.opt.scrolloff = 8                           -- minimum number of screen lines to keep above and below the cursor
+vim.opt.sidescrolloff = 8                       -- minimum number of screen lines columns to keep to the left and right of the cursor if warp is `false`
+vim.opt.guifont = "monospace:h12"               -- the font used in graphical neovim applications
+vim.opt.fillchars.eob=" "                       -- show empty lines at the end of a buffer as ` ` (default is `-`)
+vim.opt.shortmess:append "c"                    -- hide all the completion messages, e.g. "-- xxx completion (yyy)", "match 1 of 2", "The only watch", "Pattern not found"
+vim.opt.whichwrap:append("<,>,[,],h,l")         -- keys allowed to move to the previous/next line when the beginning/end of the line is reached
+vim.opt. iskeyword:append("-")                  -- treats words with `-` as single words
---
+++ old-init.vim
@@ -0,0 +1,47 @@
+set number         " set line numbers
+set autoindent     " indent new line the same amount as the line just typed
+set tabstop=4      " number of columns occupied by a tab character
+set expandtab      " convert tabs to white space
+set shiftwidth=4   " width for autoindents
+set softtabstop=4  " see multiple spaces as tabstops so <BS> does the right thing
+retab 4            " change tabs to 4 for new files
+set showmatch      " show matching brackets
+set nowrap         " always nowrap long lines
+set guioptions+=b  " set horizontal scrollbar
+syntax on
+set background=dark
+
+
+call plug#begin('~/vimfiles/plugged')
+
+Plug 'morhetz/gruvbox'
+Plug 'ryanoasis/vim-devicons'
+Plug 'vim-airline/vim-airline'
+Plug 'neoclide/coc.nvim', { 'branch': 'release' }
+
+
+call plug#end()
+
+colorscheme gruvbox
+
+" use jj to Escape to normal mode
+inoremap jk <ESC>
+
+" Use tab for trigger completion with characters ahead and navigate.
+" NOTE: There's always complete item selected by default, you may want to enable
+" no select by `"suggest.noselect": true` in your configuration file.
+" NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
+" other plugin before putting this into your config.
+inoremap <silent><expr> <TAB>
+      \ coc#pum#visible() ? coc#pum#next(1) :
+      \ CheckBackspace() ? "\<Tab>" :
+      \ coc#refresh()
+inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+
+
+" general commands
+" :help vimrc-intro   opens vim configuration help manual
+" :edit $MYVIMRC      edit the vim configuration file, can also use lowercase
+" :edit $myvimrc
+"
+"
